7060a3fbfbcecef0f4bc11d21744a21b7aebab8f
Muffcata <marta.oched@gmail.com>
2024-04-19 22:01:08 +0200

    new class Sum

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp1.zadanie_1;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
 
-Przywitanie obj = new Przywitanie();
-obj.PrzywitanieMethod();
+//Przywitanie obj = new Przywitanie();
+//obj.PrzywitanieMethod();
 
-Liczenie obj1 = new Liczenie();
-obj1.LiczenieMethod();
+//Liczenie obj1 = new Liczenie();
+//obj1.LiczenieMethod();
+
+Sumowanie obj2 = new Sumowanie();
+obj2.SumowanieMethod();
+
---
+++ ConsoleApp1/zadanie 1/Sumowanie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.zadanie_1
+{
+	internal class Sumowanie
+	{
+		public void SumowanieMethod()
+		{
+			int liczba = 20;
+
+			for (int i = 0; ; i < liczba; i++)
+			{
+				Console.WriteLine("Witaj");
+			}
+            Console.ReadKey();
+        }
+	}
+}
+
