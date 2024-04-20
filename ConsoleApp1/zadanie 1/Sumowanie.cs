2855aa63bc655e73792807490fe7d162b8454375
Muffcata <marta.oched@gmail.com>
2024-04-20 19:49:03 +0200

    zmiany w pliku

--- ConsoleApp1/zadanie 1/Sumowanie.cs
+++ ConsoleApp1/zadanie 1/Sumowanie.cs
@@ -8,15 +8,14 @@ namespace ConsoleApp1.zadanie_1
 {
 	internal class Sumowanie
 	{
-		public void SumowanieMethod()
+		static void SumowanieMethod()
 		{
-			int liczba = 20;
-
-			for (int i = 0; ; i < liczba; i++)
-			{
-				Console.WriteLine("Witaj");
-			}
-            Console.ReadKey();
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
         }
 	}
 }
