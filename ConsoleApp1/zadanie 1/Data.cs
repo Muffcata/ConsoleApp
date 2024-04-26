4d9e5872dc9e0012565c2dc218d4ac1d7d4d8cf2
m.dabrowska <m.dabrowska@inmoto.pl>
2024-04-26 14:51:11 +0200

    dodanie nowego pliku

---
+++ ConsoleApp1/zadanie 1/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.zadanie_1
+{
+    internal class Data
+    {
+        public void DataMethod()
+        {
+            Console.WriteLine("jjj");
+        }
+    }
+}
--- ConsoleApp1/zadanie 1/Sumowanie.cs
+++ ConsoleApp1/zadanie 1/Sumowanie.cs
@@ -8,14 +8,76 @@ namespace ConsoleApp1.zadanie_1
 {
     internal class Sumowanie
     {
-        public static void SumowanieMethod()
+        public void SumowanieMethod()
         {
-            int counter = 0;
-            while (counter < 10)
+            Console.WriteLine("Podaj liczbę całkowitą...");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int suma = 0;
+
+            for (int i = 1; i <= num; i++)
             {
-                Console.WriteLine($"Hello World! The counter is {counter}");
-                counter++;
+                suma += i;
             }
+
+            Console.WriteLine($"Suma liczb od 1 do {num} wynosi: {suma}");
+            Console.ReadLine();
+
+            //List<int> list = new List<int>();
+
+            //list.Add(0);
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
+            //list.Add(4);
+
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    Console.WriteLine(list[i] + list[0]);
+            //}
+            //Console.ReadLine();
+
+            //Console.WriteLine(list[1]);
+
+            //Console.ReadLine();
+
+            List<int> list = new List<int>();
+
+            list.Add(12);
+            list.Add(14);
+            list.Add(2);
+            list.Add(33);
+            list.Add(45);
+
+            Console.WriteLine("Podaj liczbę...");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            bool finder = false;
+
+            foreach (int li in list)
+            {
+                if (li == userNum)
+                {
+                    finder = true;
+                    break;
+                }
+            }
+
+            switch (finder)
+            {
+                case true:
+
+                    Console.WriteLine($"Liczba {userNum} znajduje się w liście.");
+                    break;
+
+                case false:
+
+                    Console.WriteLine($"Liczba {userNum} nie została znaleziona w liście.");
+                    break;
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
