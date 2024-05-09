1f60317504c06aafa7325e779cd2b9e3cb0e58af
m.dabrowska <m.dabrowska@inmoto.pl>
2024-05-09 08:28:18 +0200

    done exrc. reference

--- ConsoleApp1/zadanie 1/Reference.cs
+++ ConsoleApp1/zadanie 1/Reference.cs
@@ -1,33 +1,36 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace ConsoleApp1.zadanie_1
-//{
-//    internal class Reference
-//    {
-//        static void Main(string[] args)
-//        {
-//            int num3;
+namespace ConsoleApp1.zadanie_1
+{
+    internal class Reference
+    {
+        static void Main(string[] args)
+        {
+            int num3 = 0;
+            int num6;
 
-//            Console.WriteLine(PoliczSumę(num, num2, ref num3));
+            Console.WriteLine(PoliczSumę(3, 1, ref num3));
+            Console.WriteLine(PoliczSumę2(5, 3, out num6));
 
-//            int iloczyn = num * num2;
-           
-//            return iloczyn;
-         
-//        }
+        }
 
-//        static int PoliczSumę(int num, int num2, ref int num3)
-//        {
-//            int num = 1;
-//            int num2 = 3;
-//            ref int num3 = num + num2;
+        static int PoliczSumę(int num, int num2, ref int num3)
+        {
 
-//            return num3;
-//        }
+            num3 = num + num2;
 
-//    }
-//}
+            return num3;
+        }
+
+        static int PoliczSumę2(int num4, int num5, out int num6)
+        {
+            num6 = num4 + num5;
+
+            return num6;
+        }
+    }
+}
